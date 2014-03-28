a380e8fa16fcc90a1b47db159e135c35151b569e
Vishal Kadam <vishal.4kadam@gmail.com>
2014-03-28 15:08:00 -0400

    Removed code from mean branch

--- app/routes/metrics.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Products routes use metrics controller
-var metrics = require('../controllers/metric');
-
-module.exports = function(app) {
-
-    app.get('/metrics', metrics.all);
-};
